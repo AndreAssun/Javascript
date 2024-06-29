da5ae6e2d5ebe59a870ac8949babaaf966a90b50
AndreAssun <decontas@hotmail.com>
2024-06-29 09:53:01 -0300

    Construção página de filmes usando API MOVIE DB. Conclusão do layout

--- Aula10/js/script.js
+++ Aula10/js/script.js
@@ -22,18 +22,81 @@ fetch("https://api.themoviedb.org/3/movie/popular?language=pt-br&page=1&api_key=
 
         let votos = document.createElement("div");
         votos.setAttribute("class","votos")
-        votos.innerHTML = filme.vote_average;
+        votos.innerHTML = Math.round(filme.vote_average)+"%";
 
         let titulo = document.createElement("h2");
         titulo.innerHTML = filme.title;
 
         let lancamento = document.createElement("p");
         lancamento.setAttribute("class", "lancamento");
-        lancamento.innerHTML=filme.release_date;
+
+        /* Vamos utilizar o comando chamado substring, que nos ajuda a quebrar uma string com base na posição do caracter e a quantidade de caracteres
+        Por exemplo: 2024-06-29. Para pegar os quatro primeiros digitos usamos:
+        substring(0,4) */
+
+        let ano = filme.release_date.substring(0,4);
+        let mes = filme.release_date.substring(5,7);
+        let dia = filme.release_date.substring(8,10);
+
+        switch(mes){
+            case "01":
+                mes = "Jan";
+                break;
+
+            case "02":
+                mes = "Fev";
+                break;
+            
+            case "03":
+                mes = "Mar";
+                break; 
+                
+            case "04":
+                mes = "Apr";
+                break;    
+
+            case "05":
+                mes = "May";
+                break;    
+
+            case "06":
+                mes = "Jun";
+                break;
+                
+                
+            case "07":
+                mes = "Jul";
+                break; 
+
+            case "08":
+                mes = "Aug";
+                break;
+                
+            case "09":
+                mes = "Set";
+                break; 
+                
+            case "10":
+                mes = "Out";
+                break;
+
+            case "11":
+                mes = "Nov";
+                break;
+            default:
+
+            case "12":
+                mes = "Dez";
+                break;
+
+                
+        }
 
 
 
 
+
+        lancamento.innerHTML=`${dia} de ${mes} de ${ano}`;
 
 
 
